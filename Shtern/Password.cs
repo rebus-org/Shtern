bf4a7a1bfc730934109c403f2540f45efbcd2110
Mogens Heller Grabe <mookid8000@gmail.com>
2016-03-29 17:56:40 +0200

    made it

---
+++ Shtern/Password.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Shtern
+{
+    /// <summary>
+    /// Call <see cref="ReadLine"/> to read a password in from the command line
+    /// </summary>
+    public static class Password
+    {
+        /// <summary>
+        /// Reads a line from the console, outputting a mask character (default: *) for each character entered.
+        /// The mast character can be customized by setting <paramref name="maskCharacter"/> to something else
+        /// </summary>
+        public static string ReadLine(char maskCharacter = '*')
+        {
+            var enteredCharacters = new Stack<char>();
+
+            while (true)
+            {
+                var consoleKeyInfo = Console.ReadKey(true);
+
+                if (consoleKeyInfo.Key == ConsoleKey.Enter) break;
+
+                if (consoleKeyInfo.Key == ConsoleKey.Escape) return "";
+
+                if (consoleKeyInfo.Key == ConsoleKey.Backspace)
+                {
+                    if (enteredCharacters.Count > 0)
+                    {
+                        Console.CursorLeft--;
+                        Console.Write(" ");
+                        Console.CursorLeft--;
+                        enteredCharacters.Pop();
+                    }
+                    continue;
+                }
+
+                Console.Write(maskCharacter);
+
+                enteredCharacters.Push(consoleKeyInfo.KeyChar);
+            }
+
+            Console.WriteLine();
+
+            return new string(enteredCharacters.Reverse().ToArray());
+        }
+    }
+}
---
+++ scripts/build.cmd
@@ -0,0 +1,49 @@
+@echo off
+
+set version=%1
+set currentdir=%~dp0
+set root=%currentdir%\..
+set toolsdir=%root%\tools
+set nuget=%toolsdir%\NuGet\NuGet.exe
+set shterndir=%root%\Shtern
+set releasedir=%tiketdir%\bin\Release
+set deploydir=%root%\deploy
+
+if "%version%"=="" (
+	echo Please specify which version to build as a parameter.
+	echo.
+	goto exit
+)
+
+echo This will build, tag, and release version %version% of Shtern.
+echo.
+echo Please make sure that all changes have been properly committed!
+pause
+
+
+if exist "%deploydir%" (
+	echo Cleaning up old deploy dir %deploydir%
+	rd %deploydir% /s/q
+)
+
+echo Building version %version%
+
+msbuild %shterndir%\Shtern.csproj /p:Configuration=Release
+
+
+echo Packing...
+
+echo Creating deploy dir %deploydir%
+mkdir %deploydir%
+
+%nuget% pack %tiketdir%\Shtern.nuspec -OutputDirectory %deploydir% -Version %version%
+
+echo Tagging...
+
+git tag %version%
+
+echo Pushing to NuGet.org...
+
+%nuget% push %deploydir%\*.nupkg
+
+:exit
